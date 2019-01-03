b65936cd02ec79cb5ceda67efb476b1de64b9ba4
nukata <suzuki611@me.com>
2019-01-03 23:45:20 +0900

    Revised for Python 2.7 and 3.7

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ and Windows 2000/XP/7/10.  I posted it to _python-list_ on 17 June,
 2003.  It was once available at
 <http://www.oki-osk.jp/esc/python/proxy/> from 2006 to 2017.
 
+Now I have revised the proxy so that it can run on both Python 2.7 
+and Python 3.7.
+
 I hope the code of the proxy is short enough to be self-explanatory.
 If your LAN is narrow or unstable, it would be better to increase the
 default value of `max_idling` in `_read_write()` from 20 to, say, 100.
-The doc-string says that the CONNECT method has not been tested yet.
-However, I say now it has been tested heavily and works very well;
-almost every site uses `https` nowadays.  The first line of the code
-begins with `#!/bin/sh`.  The primary reason is that one of the
-targets is BeOS R5 where there is no `/usr/bin/env`.
+
+```
+$ python ./TinyHTTPProxy.py
+Any clients will be served...
+Serving HTTP/1.0 on 0.0.0.0 port 8000 ...
+
+```
--- TinyHTTPProxy.py
+++ TinyHTTPProxy.py
@@ -1,22 +1,32 @@
-#!/bin/sh -
-"exec" "python" "-O" "$0" "$@"
+#!/usr/bin/env python
 
-__doc__ = """Tiny HTTP Proxy.
+"""Tiny HTTP Proxy
 
-This module implements GET, HEAD, POST, PUT and DELETE methods
-on BaseHTTPServer, and behaves as an HTTP proxy.  The CONNECT
-method is also implemented experimentally, but has not been
-tested yet.
-
-Any help will be greatly appreciated.		SUZUKI Hisao
+This module implements GET, HEAD, POST, PUT, DELETE and CONNECT methods
+on BaseHTTPServer (or server.http), and behaves as an HTTP proxy.
 """
 
-__version__ = "0.2.1"
+from __future__ import print_function
 
-import BaseHTTPServer, select, socket, SocketServer, urlparse
+__version__ = "1.1.0"
 
-class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler):
-    __base = BaseHTTPServer.BaseHTTPRequestHandler
+import select, socket, sys
+
+try:
+    import http.server as hserv
+    from socketserver import ThreadingMixIn
+    import urllib.parse as urlparse
+    print_word = lambda wd: print(wd, end="\t", flush=True)
+    _ = lambda s: s.encode('utf-8')
+except ImportError:
+    import BaseHTTPServer as hserv
+    from SocketServer import ThreadingMixIn
+    import urlparse
+    print_word = lambda s: (print(s, end="\t"), sys.stdout.flush())
+    _ = lambda s: s
+
+class ProxyHandler (hserv.BaseHTTPRequestHandler):
+    __base = hserv.BaseHTTPRequestHandler
     __base_handle = __base.handle
 
     server_version = "TinyHTTPProxy/" + __version__
@@ -36,27 +46,30 @@ class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler):
             host_port = netloc[:i], int(netloc[i+1:])
         else:
             host_port = netloc, 80
-        print "\t" "connect to %s:%d" % host_port
+        print("\t" "connect to %s:%d" % host_port)
         try: soc.connect(host_port)
-        except socket.error, arg:
-            try: msg = arg[1]
-            except: msg = arg
+        except socket.error as arg:
+            try:
+                msg = arg[1]
+            except:
+                msg = arg
             self.send_error(404, msg)
-            return 0
-        return 1
+            return False
+        return True
 
     def do_CONNECT(self):
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             if self._connect_to(self.path, soc):
                 self.log_request(200)
-                self.wfile.write(self.protocol_version +
-                                 " 200 Connection established\r\n")
-                self.wfile.write("Proxy-agent: %s\r\n" % self.version_string())
-                self.wfile.write("\r\n")
+                self.wfile.write(_(self.protocol_version +
+                                   " 200 Connection established\r\n"))
+                self.wfile.write(_("Proxy-agent: %s\r\n" %
+                                   self.version_string()))
+                self.wfile.write(b"\r\n")
                 self._read_write(soc, 300)
         finally:
-            print "\t" "bye"
+            print_word("bye")
             soc.close()
             self.connection.close()
 
@@ -70,18 +83,19 @@ class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler):
         try:
             if self._connect_to(netloc, soc):
                 self.log_request()
-                soc.send("%s %s %s\r\n" % (
-                    self.command,
-                    urlparse.urlunparse(('', '', path, params, query, '')),
-                    self.request_version))
+                soc.send(_("%s %s %s\r\n" % (
+                            self.command,
+                            urlparse.urlunparse(('', '', path, params, query,
+                                                 '')),
+                            self.request_version)))
                 self.headers['Connection'] = 'close'
                 del self.headers['Proxy-Connection']
                 for key_val in self.headers.items():
-                    soc.send("%s: %s\r\n" % key_val)
-                soc.send("\r\n")
+                    soc.send(_("%s: %s\r\n" % key_val))
+                soc.send(b"\r\n")
                 self._read_write(soc)
         finally:
-            print "\t" "bye"
+            print_word("bye")
             soc.close()
             self.connection.close()
 
@@ -89,10 +103,11 @@ class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler):
         iw = [self.connection, soc]
         ow = []
         count = 0
-        while 1:
+        while True:
             count += 1
             (ins, _, exs) = select.select(iw, ow, iw, 3)
-            if exs: break
+            if exs:
+                break
             if ins:
                 for i in ins:
                     if i is soc:
@@ -104,30 +119,37 @@ class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler):
                         out.send(data)
                         count = 0
             else:
-                print "\t" "idle", count
-            if count == max_idling: break
+                print_word(count)
+            if count == max_idling:
+                break
 
     do_HEAD = do_GET
     do_POST = do_GET
     do_PUT  = do_GET
     do_DELETE=do_GET
 
-class ThreadingHTTPServer (SocketServer.ThreadingMixIn,
-                           BaseHTTPServer.HTTPServer): pass
+class ThreadingHTTPServer (ThreadingMixIn, hserv.HTTPServer):
+    pass
 
-if __name__ == '__main__':
-    from sys import argv
+def main(argv):
     if argv[1:] and argv[1] in ('-h', '--help'):
-        print argv[0], "[port [allowed_client_name ...]]"
+        print(argv[0], "[port [allowed_client_name ...]]")
     else:
+        server_address = ('', int(argv[1]) if argv[1:] else 8000)
         if argv[2:]:
             allowed = []
             for name in argv[2:]:
                 client = socket.gethostbyname(name)
                 allowed.append(client)
-                print "Accept: %s (%s)" % (client, name)
+                print("Accept: %s (%s)" % (client, name))
             ProxyHandler.allowed_clients = allowed
-            del argv[2:]
         else:
-            print "Any clients will be served..."
-        BaseHTTPServer.test(ProxyHandler, ThreadingHTTPServer)
+            print("Any clients will be served...")
+        httpd = ThreadingHTTPServer(server_address, ProxyHandler)
+        (host, port) = httpd.socket.getsockname()
+        print("Serving", ProxyHandler.protocol_version,
+              "on", host, "port", port, "...")
+        httpd.serve_forever()
+
+if __name__ == '__main__':
+    main(sys.argv)
